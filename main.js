d946350a20f4b094cd8bfa711c00a8fb8ec7dc6d
sparsh tiwari <sparsh.tiwariy@gmail.com>
2021-07-13 14:31:38 +0530

    changes

--- main.js
+++ main.js
@@ -1,84 +1,84 @@
-/** Heading Carousel Start **/
-(function ($) {
-  //Function to animate slider captions
-  function doAnimations(elems) {
-    //Cache the animationend event in a variable
-    var animEndEv = "webkitAnimationEnd animationend";
+  /** Heading Carousel Start **/
+  (function ($) {
+    //Function to animate slider captions
+    function doAnimations(elems) {
+      //Cache the animationend event in a variable
+      var animEndEv = "webkitAnimationEnd animationend";
 
-    elems.each(function () {
-      var $this = $(this),
-        $animationType = $this.data("animation");
-      $this.addClass($animationType).one(animEndEv, function () {
-        $this.removeClass($animationType);
+      elems.each(function () {
+        var $this = $(this),
+          $animationType = $this.data("animation");
+        $this.addClass($animationType).one(animEndEv, function () {
+          $this.removeClass($animationType);
+        });
       });
+    }
+
+    //Variables on page load
+    var $myCarousel = $("#carouselExampleIndicators"),
+      $firstAnimatingElems = $myCarousel
+        .find(".carousel-item:first")
+        .find("[data-animation ^= 'animated']");
+
+    //Initialize carousel
+    $myCarousel.carousel();
+
+    //Animate captions in first slide on page load
+    doAnimations($firstAnimatingElems);
+
+    //Other slides to be animated on carousel slide event
+    $myCarousel.on("slide.bs.carousel", function (e) {
+      var $animatingElems = $(e.relatedTarget).find(
+        "[data-animation ^= 'animated']"
+      );
+      doAnimations($animatingElems);
     });
-  }
+  })(jQuery);
+  /** Heading Carousel End **/
 
-  //Variables on page load
-  var $myCarousel = $("#carouselExampleIndicators"),
-    $firstAnimatingElems = $myCarousel
-      .find(".carousel-item:first")
-      .find("[data-animation ^= 'animated']");
+  /** Facility Start**/
 
-  //Initialize carousel
-  $myCarousel.carousel();
+  $('#myCarousel').carousel({
+    interval: 4000
+  })
 
-  //Animate captions in first slide on page load
-  doAnimations($firstAnimatingElems);
 
-  //Other slides to be animated on carousel slide event
-  $myCarousel.on("slide.bs.carousel", function (e) {
-    var $animatingElems = $(e.relatedTarget).find(
-      "[data-animation ^= 'animated']"
-    );
-    doAnimations($animatingElems);
+  /** Facility End**/
+
+
+
+
+  const wrapper = document.querySelectorAll(".cardWrap");
+
+  wrapper.forEach(element => {
+    let state = {
+      mouseX: 0,
+      mouseY: 0,
+      height: element.clientHeight,
+      width: element.clientWidth
+    };
+
+    element.addEventListener("mousemove", ele => {
+      const card = element.querySelector(".card-one");
+      const cardBg = card.querySelector(".cardBg");
+      state.mouseX = ele.pageX - element.offsetLeft - state.width / 2;
+      state.mouseY = ele.pageY - element.offsetTop - state.height / 2;
+
+      // parallax angle in card
+      const angleX = (state.mouseX / state.width) * 30;
+      const angleY = (state.mouseY / state.height) * -30;
+      card.style.transform = `rotateY(${angleX}deg) rotateX(${angleY}deg) `;
+
+      // parallax position of background in card
+      const posX = (state.mouseX / state.width) * -40;
+      const posY = (state.mouseY / state.height) * -40;
+      cardBg.style.transform = `translateX(${posX}px) translateY(${posY}px)`;
+    });
+
+    element.addEventListener("mouseout", () => {
+      const card = element.querySelector(".card-one");
+      const cardBg = card.querySelector(".cardBg");
+      card.style.transform = `rotateY(0deg) rotateX(0deg) `;
+      cardBg.style.transform = `translateX(0px) translateY(0px)`;
+    });
   });
-})(jQuery);
-/** Heading Carousel End **/
-
-/** Facility Start**/
-
-$('#myCarousel').carousel({
-  interval: 4000
-})
-
-
-/** Facility End**/
-
-
-
-
-const wrapper = document.querySelectorAll(".cardWrap");
-
-wrapper.forEach(element => {
-  let state = {
-    mouseX: 0,
-    mouseY: 0,
-    height: element.clientHeight,
-    width: element.clientWidth
-  };
-
-  element.addEventListener("mousemove", ele => {
-    const card = element.querySelector(".card-one");
-    const cardBg = card.querySelector(".cardBg");
-    state.mouseX = ele.pageX - element.offsetLeft - state.width / 2;
-    state.mouseY = ele.pageY - element.offsetTop - state.height / 2;
-
-    // parallax angle in card
-    const angleX = (state.mouseX / state.width) * 30;
-    const angleY = (state.mouseY / state.height) * -30;
-    card.style.transform = `rotateY(${angleX}deg) rotateX(${angleY}deg) `;
-
-    // parallax position of background in card
-    const posX = (state.mouseX / state.width) * -40;
-    const posY = (state.mouseY / state.height) * -40;
-    cardBg.style.transform = `translateX(${posX}px) translateY(${posY}px)`;
-  });
-
-  element.addEventListener("mouseout", () => {
-    const card = element.querySelector(".card-one");
-    const cardBg = card.querySelector(".cardBg");
-    card.style.transform = `rotateY(0deg) rotateX(0deg) `;
-    cardBg.style.transform = `translateX(0px) translateY(0px)`;
-  });
-});
